2461d4bd59e8d4f643e776a1f263a8b21626ec09
tejas <sonartejas00@gmail.com>
2021-11-14 18:00:24 +0530

    cart_page_dummy_list_created

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -19,6 +19,81 @@ class CartPage extends StatelessWidget {
           ),
         ),
       ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+                  padding: EdgeInsets.only(right: 32, left: 32),
+                  child: _CartList())),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
     );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey.shade500,
+      padding: EdgeInsets.only(left: 20, right: 15),
+      child: ButtonBar(
+        alignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "\$999",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+              fontFamily: GoogleFonts.poppins().fontFamily,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "Buy",
+              style: TextStyle(
+                fontSize: 18,
+                fontFamily: GoogleFonts.poppins().fontFamily,
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              padding:
+                  EdgeInsets.only(left: 32, right: 32, top: 12, bottom: 12),
+              shape: RoundedRectangleBorder(
+                  //to set border radius to button
+                  borderRadius: BorderRadius.circular(25)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList({Key? key}) : super(key: key);
+
+  @override
+  State<_CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) => ListTile(
+              leading: Icon(Icons.done),
+              trailing: IconButton(
+                icon: Icon(Icons.remove),
+                onPressed: () {},
+              ),
+              title: Text("Item 1"),
+            ));
   }
 }
