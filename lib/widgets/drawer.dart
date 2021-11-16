1b617dd785c345956fcad30734bb7ed42edc98d7
tejas <sonartejas00@gmail.com>
2021-11-16 22:30:35 +0530

    all done

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,13 +7,14 @@ class MyDrawer extends StatelessWidget {
   const MyDrawer({Key? key}) : super(key: key);
 
   final imageUrl =
-      "https://www.google.com/search?q=god+of+war&sxsrf=AOaemvJ-hNqrc8DWZgupWz-ajPY0NqyMWg:1636354796677&source=lnms&tbm=isch&sa=X&ved=2ahUKEwim-YbTmIj0AhXSV30KHbksA9YQ_AUoAnoECAEQBA&biw=1812&bih=872&dpr=1.06#imgrc=se5H6fX_95v1YM";
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcharacters.fandom.com%2Fwiki%2FEren_Yeager&psig=AOvVaw0pVEJsvmnqah8MaiqPZbvR&ust=1637167585983000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOiP7teqnfQCFQAAAAAdAAAAABAD";
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         children: [
+          // ignore: prefer_const_constructors
           DrawerHeader(
             padding: EdgeInsets.zero,
             child: UserAccountsDrawerHeader(
@@ -19,7 +22,7 @@ class MyDrawer extends StatelessWidget {
               accountName: const Text("Tejas Sonar"),
               accountEmail: const Text("sonartejas00@gmail.com"),
               currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(imageUrl),
+                backgroundImage: AssetImage("assets/images/profile.jpg"),
               ),
             ),
           ),
